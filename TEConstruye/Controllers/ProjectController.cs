26b99fac905588b849e30520dcc027e7698a0a34
geraldvm <gerald-_16@hotmail.com>
2019-11-06 00:17:11 -0600

    c

--- TEConstruye/Controllers/ProjectController.cs
+++ TEConstruye/Controllers/ProjectController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +11,35 @@ namespace TEConstruye.Controllers
 {
     public class ProjectController : ApiController
     {
+        NpgsqlConnection conn = new NpgsqlConnection(BDconnection.conn);
+        [HttpGet]
+        [Route("api/Projects")]
+        public HttpResponseMessage GetProjects()
+        {
+            conn.Open();
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("SELECT id, lotarea,rooms,restrooms," +
+                "floors, builtarea, client_username,client_name, province, canton, district" +
+                " SELECT CONCAT(province, ',', canton,',',district) AS address FROM public.vProject", conn);
+            DataSet list = new DataSet();
+            adapter.Fill(list);
+            conn.Close();
+            return this.Request.CreateResponse(HttpStatusCode.OK, list.Tables[0]);
+
+        }
+
+        [HttpPut]
+        [Route("api/Project")]
+        public HttpResponseMessage AddProject()
+        {
+            conn.Open();
+            NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("SELECT id, lotarea,rooms,restrooms," +
+                "floors, builtarea, client_username,client_name, province, canton, district" +
+                " SELECT CONCAT(province, ',', canton,',',district) AS address FROM public.vProject", conn);
+            DataSet list = new DataSet();
+            adapter.Fill(list);
+            conn.Close();
+            return this.Request.CreateResponse(HttpStatusCode.OK, list.Tables[0]);
+
+        }
     }
 }
