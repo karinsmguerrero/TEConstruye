fce36840b24d9d83e2ad8e9eed9586d030c685ab
Karina Martinez Guerrero <karinamguerrero98@gmail.com>
2019-11-05 22:39:47 -0600

    Rm cached

---
+++ TEConstruye/BDconnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEConstruye
+{
+    public class BDconnection
+    {
+        public static string conn = "Server=localhost;Port=5432; User Id = postgres;Password = karina;Database = TEConstruye2";
+    }
+}
---
+++ TEConstruye/Models/PropiedadU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEConstruye.Models
+{
+    public class PropiedadU
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Bathroom { get; set; }
+        public string Type { get; set; }
+        public int LotArea { get; set; }
+        public int BuiltArea { get; set; }
+        public bool Pools { get; set; }
+        public bool Parkings { get; set; }
+        public int Floors { get; set; }
+        public int Rooms { get; set; }
+        public int Price { get; set; }
+        public bool Gym { get; set; }
+        public string Location { get; set; }
+        public string Descripcion { get; set; }
+        public string Cover { get; set; }
+    }
+}
