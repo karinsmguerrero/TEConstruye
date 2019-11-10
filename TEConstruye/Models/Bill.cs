352c91509f6a2d18c4e19139c0b9829159180e04
geraldvm <gerald-_16@hotmail.com>
2019-11-09 18:39:42 -0600

    Models

---
+++ TEConstruye/Models/Bill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEConstruye.Models
+{
+    public class Bill
+    {
+        public string  provider{ get; set; }
+        public int billnumber { get; set; }
+        public string billphoto { get; set; }
+        public int idstage { get; set; }
+    }
+}
---
+++ TEConstruye/Models/WorkerOnProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEConstruye.Models
+{
+    public class WorkerOnProject
+    {
+       
+        public int idstage { get; set; }
+        public float hours { get; set; }
+        public string username { get; set; }
+    }
+}
