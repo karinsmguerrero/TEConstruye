3e8d25c909b55823a59570cf100743fa40185a3c
geraldvm <gerald-_16@hotmail.com>
2019-11-10 18:26:12 -0600

    new services

--- src/app/Services/stage-type-management.service.ts
+++ src/app/Services/stage-type-management.service.ts
@@ -3,7 +3,7 @@ import { Stage } from '../Models/stage.model';
 import { ConstantsService } from './constants.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
-
+import  {ToastrService} from 'ngx-toastr';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +12,7 @@ export class StageTypeManagementService {
     
   list : Stage[];
   
-  constructor(private http : HttpClient, private constant: ConstantsService) { }
+  constructor(private http : HttpClient, private constant: ConstantsService, private toastr :ToastrService) { }
   getStagesType(){
     this.http.get(this.constant.routeURL + '/GetStagesType').toPromise().then(res => this.list = res as Stage[]);
   }
@@ -29,7 +29,11 @@ export class StageTypeManagementService {
       name:  formData.value.Name,
       description: formData.value.Description
     };
-    this.http.post(this.constant.routeURL + '/PostStageType',body,httpOptions).toPromise();
+    this.http.post(this.constant.routeURL + '/PostStageType',body,httpOptions).subscribe(res =>{
+      this.toastr.success('Successfull','Tipo de etapa agregada')}, error=> {
+        this.toastr.error('Error','Error al registrar tipo de etapa');
+      }
+    );
     
   }
 }
