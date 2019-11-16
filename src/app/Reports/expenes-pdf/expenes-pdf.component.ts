bf48fbdbdbb2b682daca1307ec5f7d7bb4bce46a
Karina Martinez Guerrero <karinamguerrero98@gmail.com>
2019-11-15 18:05:10 -0600

    reportes

---
+++ src/app/Reports/expenes-pdf/expenes-pdf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expenes-pdf',
+  templateUrl: './expenes-pdf.component.html',
+  styleUrls: ['./expenes-pdf.component.css']
+})
+export class ExpenesPdfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Reports/salaries-pdf/salaries-pdf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-salaries-pdf',
+  templateUrl: './salaries-pdf.component.html',
+  styleUrls: ['./salaries-pdf.component.css']
+})
+export class SalariesPdfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
